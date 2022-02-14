62e02c629d2b462fa52e16952a142fea888e4d7c
Joe Linogao <linogaoj@tcd.ie>
2022-02-14 18:15:10 +0000

    setup jovian api

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+# assignment 1 from jovian
+
+# jovian project can be found here: https://jovian.ai/joecey/python-binary-search-assignment
+
+import jovian
+print("packages imported")
+
+project='python-binary-search-assignment'
+jovian.commit(project=project, privacy='secret', environment=None)
+
+# sorted list = ascending order
+# rotating a list = taking number at end of list and putting it to the front
+
+# for a sorted list and a given list, figure out how many rotations it took to
+# turn the given list into the sorted list
